578eaca1aaa5f7b406467ac3f77cc7da530d3725
shahi.subash79@gmail.com <shahi.subash79@gmail.com>
2024-05-19 12:40:19 +0930

    CB-02-001 XSS attacks via script injection in message field

--- views/display.py
+++ views/display.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template, session, redirect, url_for, request
+import re
+from flask import Blueprint, render_template, session, redirect, url_for, jsonify, request
 from logic.forms import (
     MessageForm,
     TransferForm,
@@ -11,12 +12,18 @@ import json
 import requests
 
 app = Blueprint('display', __name__, template_folder='templates')
-passs = lambda k,v : k[v]
+def passs(k, v): return k[v]
+
+
+def is_valid_input(input_string):
+    return re.match("^[A-Za-z0-9 ]*$", input_string) is not None
+
 
 @app.route("/list")
 def list_accounts():
     accounts = Account.query.filter_by(active=True)
     return render_template("list_accounts.html", accounts=accounts)
+
 
 @app.route("/account", methods=["GET", "POST"])
 def my_account():
@@ -25,17 +32,24 @@ def my_account():
 
     user = session["username"]
     account = Account.query.filter_by(name=user).first()
-    transactions = Transaction.query.filter_by(account_id=account.id).order_by(Transaction.date.desc())
+    transactions = Transaction.query.filter_by(
+        account_id=account.id).order_by(Transaction.date.desc())
 
     withdraw_form = WithdrawForm()
     message_form = MessageForm()
     transfer_form = TransferForm()
+    error_message = ""
 
     if message_form.validate_on_submit():
-        messageDb = Messages(account_id=account.id, message=message_form.message_text.data)
-        db.session.add(messageDb)
-        db.session.commit()
-        return redirect(url_for("display.my_account"))
+        if not is_valid_input(message_form.message_text.data):
+            print('Invalid text detected')
+            error_message = "Invalid input"
+        else:
+            messageDb = Messages(account_id=account.id,
+                                 message=message_form.message_text.data)
+            db.session.add(messageDb)
+            db.session.commit()
+            return redirect(url_for("display.my_account"))
 
     if withdraw_form.validate_on_submit():
         if account.deposit_withdraw("withdraw", withdraw_form.amount.data):
@@ -79,8 +93,8 @@ def my_account():
         withdraw_form=withdraw_form,
         message_form=message_form,
         transfer_form=transfer_form,
+        error_message=error_message
     )
-
 
 
 @app.route("/delete", methods=["GET", "POST"])
@@ -105,6 +119,7 @@ def delete_account():
 @app.route("/pong", methods=["GET"])
 def pong():
     return "pong"
+
 
 @app.route("/ping", methods=["GET"])
 def ping():
